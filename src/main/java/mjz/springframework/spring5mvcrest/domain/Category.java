79ad43f26c2426d28e04a2d941335144ac5dd5e0
MajidJowDev <majid.jow@gmail.com>
2022-09-20 17:50:33 +0430

    24- 3. Spring Boot Command Line Runner - Added bootstrap class (data initiator) using CommandLineRunner

--- src/main/java/mjz/springframework/spring5mvcrest/domain/Category.java
+++ src/main/java/mjz/springframework/spring5mvcrest/domain/Category.java
@@ -2,11 +2,13 @@ package mjz.springframework.spring5mvcrest.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity // if we do not include this annotation the context building will be failed
 public class Category {
 
     @Id
