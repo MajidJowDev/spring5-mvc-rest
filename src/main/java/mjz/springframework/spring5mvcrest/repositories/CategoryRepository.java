0bbba5f62f0161a59cf8d9b284acc0899276ef4b
MajidJowDev <majid.jow@gmail.com>
2022-09-20 17:30:42 +0430

    24- 2. New Spring Boot Project... setting up POJO, API DTO and Repository for Category object

---
+++ src/main/java/mjz/springframework/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package mjz.springframework.spring5mvcrest.repositories;
+
+import mjz.springframework.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
