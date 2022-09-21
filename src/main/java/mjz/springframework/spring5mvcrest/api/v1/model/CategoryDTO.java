710440e614ac14003f9b9b4476c08ee5d0d98ac5
MajidJowDev <majid.jow@gmail.com>
2022-09-21 09:09:30 +0430

    24- 5. Using MapStruct... added Mapper interface and annotating it with MapStruct @Mapper and adjust the CategoryDTO class by adding lombok @Data to make the mapper able to auto generate the impl class

--- src/main/java/mjz/springframework/spring5mvcrest/api/v1/model/CategoryDTO.java
+++ src/main/java/mjz/springframework/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -1,11 +1,11 @@
 package mjz.springframework.spring5mvcrest.api.v1.model;
 
-
+import lombok.Data;
 // A DTO Class (Data Transfer Object) is like data structure, which should not contain any business logic
 //but should contain Serialization and deserialization mechanisms, they can have all or partial data from data source
 // they can hold data from single or multiple sources as well
-
 // we are going to expose CategoryDTO through the REST Controller, so we need to set up a mapping this to the Category Controller and Catagory POJO
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
---
+++ src/test/java/mjz/springframework/spring5mvcrest/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,31 @@
+package mjz.springframework.spring5mvcrest.api.v1.mapper;
+
+import mjz.springframework.spring5mvcrest.api.v1.model.CategoryDTO;
+import mjz.springframework.spring5mvcrest.domain.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+
+    public static final String NAME = "Joe";
+    public static final long ID = 1L;
+
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;  // just a helper
+
+    @Test
+    void categoryToCategoryDTO() {
+
+        //given
+        Category category = new Category();
+        category.setName(NAME);
+        category.setId(ID);
+
+        //when
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        //then
+        assertEquals(Long.valueOf(ID), categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
