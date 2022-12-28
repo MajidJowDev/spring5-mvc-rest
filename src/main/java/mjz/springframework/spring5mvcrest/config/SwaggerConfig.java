bb7234395219e66aa11133181831abb8cbaa8586
MajidJowDev <majid.jow@gmail.com>
2022-12-28 17:04:48 +0330

    25- 5. Customizing Swagger with Meta Data... added metadata (ApiInfo) to the swagger ui

--- src/main/java/mjz/springframework/spring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/mjz/springframework/spring5mvcrest/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2  //this annotation brings swagger support to the project
 @Configuration
@@ -27,7 +31,8 @@ public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ { // if we did
                 .apis(RequestHandlerSelectors.any()) // set for any APIs or requestHandlers
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData()); // adds the api information to swagger UI
     }
 
     //if we are not in a spring boot project we should add our resources manually like below
@@ -43,4 +48,21 @@ public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ { // if we did
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
     */
+
+    // added this metadata to the swagger UI
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Majid Xoqi", "https://fiverr.com/itdev_solutions",
+                "majid.jow@gmail.com");
+
+        return new ApiInfo(
+                "My RestFul API Example",
+                "Spring Framework 5: RestFul API Examples",
+                "1.0",
+                "Terms of Service: My terms",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
 }
