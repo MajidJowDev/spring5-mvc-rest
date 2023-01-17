f8f951ce50bf07a48bd5bf2e1784c6ee34bf13f7
MajidJowDev <majid.jow@gmail.com>
2023-01-18 01:01:08 +0330

    25- 7. Assignment Customize Vendors

--- src/main/java/mjz/springframework/spring5mvcrest/api/v1/model/VendorDTO.java
+++ src/main/java/mjz/springframework/spring5mvcrest/api/v1/model/VendorDTO.java
@@ -1,6 +1,7 @@
 package mjz.springframework.spring5mvcrest.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class VendorDTO {
 
+    @ApiModelProperty(value = "Name of the Venodr", required = true)
     private String name;
 
+    @ApiModelProperty(readOnly = true)
     @JsonProperty("vendor_url")
     private String vendorUrl;
 }
--- src/main/java/mjz/springframework/spring5mvcrest/controllers/v1/VendorController.java
+++ src/main/java/mjz/springframework/spring5mvcrest/controllers/v1/VendorController.java
@@ -1,11 +1,14 @@
 package mjz.springframework.spring5mvcrest.controllers.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import mjz.springframework.spring5mvcrest.api.v1.model.VendorDTO;
 import mjz.springframework.spring5mvcrest.api.v1.model.VendorListDTO;
 import mjz.springframework.spring5mvcrest.services.VendorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This endpoint consists of operations related to Vendor controller")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -18,12 +21,14 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "Returns a list of Vendors", notes = "this methods needs no parameters")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getAllVendors(){
         return new VendorListDTO(vendorService.getAllVendors());
     }
 
+    @ApiOperation(value = "Returns Vendor info based on received Vendor ID ", notes = "Send vendor ID")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendorById(@PathVariable Long id){
