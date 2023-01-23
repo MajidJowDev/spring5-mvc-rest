e9b7eb96e9ebdffb07caacb9119fc9335d4d5fe8
MajidJowDev <majid.jow@gmail.com>
2023-01-23 04:02:13 +0330

    27- 7. Refactor for JAXB Generated Classes - 8. Testing and Running JAXB Generated Classes in Spring Boot Application ... These parts are left due to the huge changes needed for CustomerDTO from the manual way to JAXB way

--- spring5-mvc-rest-app/src/main/java/mjz/springframework/spring5mvcrest/services/CustomerServiceImpl.java
+++ spring5-mvc-rest-app/src/main/java/mjz/springframework/spring5mvcrest/services/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package mjz.springframework.spring5mvcrest.services;
 
 import mjz.springframework.spring5mvcrest.api.v1.mapper.CustomerMapper;
 import mjz.springframework.spring5mvcrest.api.v1.model.CustomerDTO;
+//import mjz.springframework.model.CustomerDTO;
 import mjz.springframework.spring5mvcrest.controllers.v1.CustomerController;
 import mjz.springframework.spring5mvcrest.domain.Customer;
 import mjz.springframework.spring5mvcrest.repositories.CustomerRepository;
